a24e8013d8658981b2273607ec3140239eb91227
ernanilima <ernani.tecc@gmail.com>
2022-06-02 15:21:12 -0300

    Bibliotecas no pom.xml

--- src/main/java/br/com/ernanilima/jcep/JCep.java
+++ src/main/java/br/com/ernanilima/jcep/JCep.java
@@ -1,9 +1,10 @@
 package br.com.ernanilima.jcep;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class JCep {
-
     public static void main(String[] args) {
         SpringApplication.run(JCep.class, args);
     }
