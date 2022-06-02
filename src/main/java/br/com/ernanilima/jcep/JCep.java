5b56b668a8e0577127b419e102f15555e7f06b52
ernanilima <ernani.tecc@gmail.com>
2022-06-02 15:08:23 -0300

    Aplicacao criada

---
+++ src/main/java/br/com/ernanilima/jcep/JCep.java
@@ -0,0 +1,10 @@
+package br.com.ernanilima.jcep;
+
+import org.springframework.boot.SpringApplication;
+
+public class JCep {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JCep.class, args);
+    }
+}
