8eb7e5f148bf2115a12b0e8e048eacde782becb9
ernanilima <ernani.tecc@gmail.com>
2022-09-18 21:15:13 -0300

    Ajustes

--- src/main/java/br/com/ernanilima/jcep/config/profiles/ConfDevPersist.java
+++ src/main/java/br/com/ernanilima/jcep/config/profiles/ConfDevPersist.java
@@ -1,11 +1,17 @@
 package br.com.ernanilima.jcep.config.profiles;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "br.com.ernanilima.jcep.repository")
 @Profile("dev")
 public class ConfDevPersist extends ConfProfilesDevProd {
 
--- src/main/java/br/com/ernanilima/jcep/config/profiles/ConfProfilesDevProd.java
+++ src/main/java/br/com/ernanilima/jcep/config/profiles/ConfProfilesDevProd.java
@@ -1,24 +1,18 @@
 package br.com.ernanilima.jcep.config.profiles;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.Properties;
 
-@Configuration
-@EnableTransactionManagement
-@EnableJpaRepositories(basePackages = "br.com.ernanilima.jcep.repository")
 public abstract class ConfProfilesDevProd {
 
     private DriverManagerDataSource dataSource;
@@ -56,14 +50,14 @@ public abstract class ConfProfilesDevProd {
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(emf);
         return transactionManager;
     }
 
     @Bean
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
         return new PersistenceExceptionTranslationPostProcessor();
     }
 }
