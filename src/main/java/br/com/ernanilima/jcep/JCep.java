ed6544252738587ed378d17856a1c670aa4f3fa0
ernanilima <ernani.tecc@gmail.com>
2022-09-22 23:04:38 -0300

    SQL e changeset para estados

--- src/main/java/br/com/ernanilima/jcep/JCep.java
+++ src/main/java/br/com/ernanilima/jcep/JCep.java
@@ -1,24 +1,14 @@
 package br.com.ernanilima.jcep;
 
-import br.com.ernanilima.jcep.utils.DataBase;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JCep extends SpringBootServletInitializer implements CommandLineRunner {
-
-    @Autowired
-    private DataBase dataBase;
+public class JCep extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JCep.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        // dataBase.createDataDatabase();
-    }
 }
---
+++ src/main/resources/db/sql/state.sql
@@ -0,0 +1,36 @@
+-- NORTE
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Rondônia', 'RO', 11, ${f_id_country_BR}, ${f_id_region_NORTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Acre', 'AC', 12, ${f_id_country_BR}, ${f_id_region_NORTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Amazonas', 'AM', 13, ${f_id_country_BR}, ${f_id_region_NORTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Roraima', 'RR', 14, ${f_id_country_BR}, ${f_id_region_NORTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Pará', 'PA', 15, ${f_id_country_BR}, ${f_id_region_NORTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Amapá', 'AP', 16, ${f_id_country_BR}, ${f_id_region_NORTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Tocantins', 'TO', 17, ${f_id_country_BR}, ${f_id_region_NORTE});
+
+-- NORDESTE
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Maranhão', 'MA', 21, ${f_id_country_BR}, ${f_id_region_NORDESTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Piauí', 'PI', 22, ${f_id_country_BR}, ${f_id_region_NORDESTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Ceará', 'CE', 23, ${f_id_country_BR}, ${f_id_region_NORDESTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Rio Grande do Norte', 'RN', 24, ${f_id_country_BR}, ${f_id_region_NORDESTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Paraíba', 'PB', 25, ${f_id_country_BR}, ${f_id_region_NORDESTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Pernambuco', 'PE', 26, ${f_id_country_BR}, ${f_id_region_NORDESTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Alagoas', 'AL', 27, ${f_id_country_BR}, ${f_id_region_NORDESTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Sergipe', 'SE', 28, ${f_id_country_BR}, ${f_id_region_NORDESTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Bahia', 'BA', 29, ${f_id_country_BR}, ${f_id_region_NORDESTE});
+
+-- SUDESTE
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Minas Gerais', 'MG', 31, ${f_id_country_BR}, ${f_id_region_SUDESTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Espírito Santo', 'ES', 32, ${f_id_country_BR}, ${f_id_region_SUDESTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Rio de Janeiro', 'RJ', 33, ${f_id_country_BR}, ${f_id_region_SUDESTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'São Paulo', 'SP', 35, ${f_id_country_BR}, ${f_id_region_SUDESTE});
+
+-- SUL
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Paraná', 'PR', 41, ${f_id_country_BR}, ${f_id_region_SUL});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Santa Catarina', 'SC', 42, ${f_id_country_BR}, ${f_id_region_SUL});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Rio Grande do Sul', 'RS', 43, ${f_id_country_BR}, ${f_id_region_SUL});
+
+-- CENTRO_OESTE
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Mato Grosso do Sul', 'MS', 50, ${f_id_country_BR}, ${f_id_region_CENTRO_OESTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Mato Grosso', 'MT', 51, ${f_id_country_BR}, ${f_id_region_CENTRO_OESTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Goiás', 'GO', 52, ${f_id_country_BR}, ${f_id_region_CENTRO_OESTE});
+INSERT INTO state (id_state , name, acronym, code, country_id, region_id) VALUES (${uuid_default}, 'Distrito Federal', 'DF', 53, ${f_id_country_BR}, ${f_id_region_CENTRO_OESTE});
