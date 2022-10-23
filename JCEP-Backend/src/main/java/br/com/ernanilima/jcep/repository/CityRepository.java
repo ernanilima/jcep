2a9228a48a3fe75bc4f2dcc0ad05a1c3eb3c2b50
ernanilima <ernani.tecc@gmail.com>
2022-10-23 13:20:43 -0300

    Busca paginada e teste para CityRepository

--- JCEP-Backend/src/main/java/br/com/ernanilima/jcep/repository/CityRepository.java
+++ JCEP-Backend/src/main/java/br/com/ernanilima/jcep/repository/CityRepository.java
@@ -1,7 +1,11 @@
 package br.com.ernanilima.jcep.repository;
 
 import br.com.ernanilima.jcep.domain.City;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,4 +16,17 @@ import java.util.UUID;
 @Transactional(readOnly = true)
 public interface CityRepository extends JpaRepository<City, UUID> {
     Optional<City> findByCode(Integer code);
+
+    @Query("SELECT city FROM City city " +
+            "JOIN city.country country " +
+            "JOIN city.region region " +
+            "JOIN city.state state " +
+            "WHERE lower(country.acronym) = lower(:countryAcronym) " +
+            "AND (:regionName IS NULL OR lower(region.name) = lower(:regionName)) " +
+            "AND (:stateAcronym IS NULL OR lower(state.acronym) = lower(:stateAcronym))")
+    Page<City> findAllByCountry_AcronymAndRegion_NameOrState_Acronym(
+            @Param("countryAcronym") String countryAcronym,
+            @Param("regionName") String regionName,
+            @Param("stateAcronym") String stateAcronym,
+            Pageable pageable);
 }
--- JCEP-Backend/src/test/java/br/com/ernanilima/jcep/repository/CityRepositoryTestIT.java
+++ JCEP-Backend/src/test/java/br/com/ernanilima/jcep/repository/CityRepositoryTestIT.java
@@ -1,10 +1,13 @@
 package br.com.ernanilima.jcep.repository;
 
+import br.com.ernanilima.jcep.builder.PageableBuilder;
 import br.com.ernanilima.jcep.config.annotation.RepositoryTest;
 import br.com.ernanilima.jcep.domain.City;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Optional;
@@ -34,5 +37,93 @@ class CityRepositoryTestIT {
         assertNotNull(city.get().getCountry());
         assertNotNull(city.get().getRegion());
         assertNotNull(city.get().getState());
+    }
+
+    @Test
+    @DisplayName("Deve retornar as cidades para a sigla do país")
+    void findAllByCountry_Acronym_Must_Return_Cities_To_Country_Acronym() {
+        Pageable pageable = PageableBuilder.create();
+
+        Page<City> cities = cityRepository.findAllByCountry_AcronymAndRegion_NameOrState_Acronym(
+                "BR", null, null, pageable);
+
+        assertNotNull(cities);
+        assertEquals(cities.getTotalElements(), 5570L);
+        assertEquals(cities.getContent().size(), 12);
+        assertEquals(cities.getTotalPages(), 465);
+    }
+
+    @Test
+    @DisplayName("Deve retornar as cidades para o nome da regiao")
+    void findAllByCountry_AcronymAndRegion_Name_Must_Return_Cities_To_Region_Name() {
+        Pageable pageable = PageableBuilder.create();
+
+        Page<City> cities = cityRepository.findAllByCountry_AcronymAndRegion_NameOrState_Acronym(
+                "BR", "NORTE", null, pageable);
+
+        assertNotNull(cities);
+        assertEquals(cities.getTotalElements(), 450L);
+        assertEquals(cities.getContent().size(), 12);
+        assertEquals(cities.getTotalPages(), 38);
+    }
+
+    @Test
+    @DisplayName("Deve retornar as cidades para a sigla do estado")
+    void findAllByCountry_AcronymAndState_Acronym_Must_Return_Cities_To_State_Acronym() {
+        Pageable pageable = PageableBuilder.create();
+
+        Page<City> cities = cityRepository.findAllByCountry_AcronymAndRegion_NameOrState_Acronym(
+                "BR", null, "PA", pageable);assertNotNull(cities);
+        assertEquals(cities.getTotalElements(), 144L);
+        assertEquals(cities.getContent().size(), 12);
+        assertEquals(cities.getTotalPages(), 12);
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma busca vazia por passar valores em branco")
+    void findAllByEmptyValues_Must_Return_An_Empty_Search_By_Passing_Empty_Values() {
+        Pageable pageable = PageableBuilder.create();
+
+        Page<City> cities = cityRepository.findAllByCountry_AcronymAndRegion_NameOrState_Acronym(
+                "", "", "", pageable);
+
+        assertNotNull(cities);
+        assertEquals(cities.getTotalElements(), 0L);
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma busca vazia por passar valores nulos")
+    void findAllByNullValues_Must_Return_An_Empty_Search_By_Passing_Null_Values() {
+        Pageable pageable = PageableBuilder.create();
+
+        Page<City> cities = cityRepository.findAllByCountry_AcronymAndRegion_NameOrState_Acronym(
+                null, null, null, pageable);
+
+        assertNotNull(cities);
+        assertEquals(cities.getTotalElements(), 0L);
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma busca vazia por passar apenas o nome da regia")
+    void findAllByRegion_Name_Must_Return_An_Empty_Search_By_Passing_Only_The_Region_Name() {
+        Pageable pageable = PageableBuilder.create();
+
+        Page<City> cities = cityRepository.findAllByCountry_AcronymAndRegion_NameOrState_Acronym(
+                null, "NORTE", null, pageable);
+
+        assertNotNull(cities);
+        assertEquals(cities.getTotalElements(), 0L);
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma busca vazia por passar apenas a sigla do estado")
+    void findAllByState_Acronym_Must_Return_An_Empty_Search_By_Passing_Only_The_State_Acronym() {
+        Pageable pageable = PageableBuilder.create();
+
+        Page<City> cities = cityRepository.findAllByCountry_AcronymAndRegion_NameOrState_Acronym(
+                null, null, "PA", pageable);
+
+        assertNotNull(cities);
+        assertEquals(cities.getTotalElements(), 0L);
     }
 }
